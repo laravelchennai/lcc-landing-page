b72e37128b4c3f9320881bc6afdf5088f70d1aa8
Thirumalai15 <m.thirurahul@gmail.com>
2025-01-30 23:40:20 +0530

    [ADD] Web routes and vadivelu themed coming soon page added

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -20,11 +20,11 @@
         <header class="absolute inset-x-0 top-0 z-50">
             <nav class="flex items-center justify-center p-8">
                 <a href="#" class="block">
-                    <img class="h-16 w-auto mx-auto" src="{{ asset('laravel_chennai_logo.png') }}"
-                        alt="Laravel Chennai Logo">
+                   <img class="h-16 w-auto mx-auto" src="{{ asset('laravel_chennai_logo.png') }}" alt="Laravel Chennai Logo">
                 </a>
             </nav>
         </header>
+
 
         <!-- Background Image -->
         <div class="absolute inset-x-0 bottom-0 opacity-[0.3] pointer-events-none fixed">
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,70 @@ Route::get('/', function () {
 });
 
 
-Route::get('/we', function () {
-    return view('welcome');
+
+// Offline Meetups
+Route::prefix('meetups')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/', [MeetupController::class, 'index'])->name('meetups.index');
+//    Route::get('/partners', [MeetupController::class, 'partners'])->name('meetups.partners');
 });
+
+// Membership
+Route::prefix('membership')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/register', [MembershipController::class, 'create'])->name('membership.register');
+//    Route::post('/register', [MembershipController::class, 'store'])->name('membership.store');
+//    Route::get('/list', [MembershipController::class, 'index'])->name('membership.list');
+});
+
+
+// Services
+Route::prefix('services')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/fcto', [ServiceController::class, 'fcto'])->name('services.fcto');
+//    Route::get('/kaapathunga-gurunadha', [ServiceController::class, 'kaapathunga'])->name('services.kaapathunga');
+//    Route::post('/book', [ServiceController::class, 'book'])->name('services.book');
+});
+
+
+// Talk Submission & Requests
+Route::prefix('talks')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/submit', [TalkController::class, 'create'])->name('talks.submit');
+//    Route::post('/submit', [TalkController::class, 'store'])->name('talks.store');
+//    Route::get('/request', [TalkController::class, 'requestForm'])->name('talks.request');
+//    Route::post('/request', [TalkController::class, 'submitRequest'])->name('talks.request.submit');
+});
+
+// Blogs
+Route::prefix('blog')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+//    Route::get('/{slug}', [BlogController::class, 'show'])->name('blog.show');
+});
+
+// resources for beginners
+Route::prefix('resources')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+//    Route::get('/{slug}', [BlogController::class, 'show'])->name('blog.show');
+//    Route::get('/video-series', [VideoSeriesController::class, 'show'])->name('blog.show');
+    //
+});
+
+Route::prefix('partners')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+//    Route::get('/{slug}', [BlogController::class, 'show'])->name('blog.show');
+});
+
+Route::prefix('forum')->group(function () {
+    return view('vadivelu-coming-soon');
+//    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+//    Route::get('/{slug}', [BlogController::class, 'show'])->name('blog.show');
+});
+
+Route::prefix('talks')->group(function () {
+    return view('vadivelu-coming-soon');
+});
+
