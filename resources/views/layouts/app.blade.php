dadcf482d48a964b4e46f8b360b83b699217fc3c
Thirumalai15 <m.thirurahul@gmail.com>
2025-02-12 23:58:56 +0530

    minor fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,29 +23,3 @@
 @yield('js')
 </body>
 </html>
-
-
-{{--updates
-    Membership application completed, now members can signup using the form with all the details and their details will be stored in the
-    memberships table instead of users table
-
-    filament admin panel added the membership  has the following status
-    'APPLIED';
-    'UNDER_REVIEW';
-    'APPROVED';
-    'ACTIVE';
-    'SUSPENDED';
-    'REJECTED';
-
-    and if an membership is approved an email will be sent to the member stating that their membership form is approved
-
-
-   Blogging module
-   installed wink blogging package https://github.com/themsaid/wink  and this is a sperate application and uses seprate database to store the users so we have create new author
-   which will be stored in the wink_authors table
-
-    created a simple fronted to view the blogs, the looks similar to blog.laravel.com
-    disqus comments has been added for the blogs
-
-
---}}
